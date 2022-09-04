67ce449122da1b709cd77aaff6778356f2d49857
Mansi Padgelwar <mugdhapadgelwar2002@gmail.com>
2022-09-04 21:03:33 +0530

    initial commit

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector(".form-container");
+const submitBtn = document.querySelector("#submit-btn");
+const output = document.querySelector("#output");
+
+const correctAnswers = ["90", "right angled"];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResult = new FormData(quizForm);
+  for (let value of formResult.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+
+  output.innerText = "your score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
