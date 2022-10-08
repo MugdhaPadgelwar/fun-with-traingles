865eec1545b7eb0e677d5e5537a428576b66cea4
Mansi Padgelwar <mugdhapadgelwar2002@gmail.com>
2022-10-08 11:03:51 +0530

    fix: handle empty zero negative value

--- isTriangle.js
+++ isTriangle.js
@@ -3,38 +3,31 @@ const isTraingleBtn = document.querySelector("#check-btn");
 const output = document.querySelector("#output");
 
 function calculateSum(angle1, angle2, angle3) {
-  if (angle1 != "" && angle2 != "" && angle3 != "") {
-    if (angle1 < 0 && angle2 < 0 && angle3 < 0) {
-      output.innerText = "Input should be greater than 0";
-    } else {
-      const sumOfAngles = angle1 + angle2 + angle3;
-      return sumOfAngles;
-    }
-  } else {
-    output.innerText = "Please enter all the inputs!";
-  }
-
-  // if (angle1 != "" || angle2 != "" || angle3 != "") {
-  //   output.innerText = "Please enter all the inputs!";
-  // } else if (angle1 >= 0 || angle2 >= 0 || angle3 >= 0) {
-  //   const sumOfAngles = angle1 + angle2 + angle3;
-  //   return sumOfAngles;
-  // } else {
-  //   output.innerText = "Input should be greater than 0";
-  // }
+  const sumOfAngles = angle1 + angle2 + angle3;
+  return sumOfAngles;
 }
 
 function isTriangle() {
-  const sumOfAngles = calculateSum(
-    Number(inputs[0].value),
-    Number(inputs[1].value),
-    Number(inputs[2].value)
-  );
-
-  if (sumOfAngles === 180) {
-    output.innerText = "Yay! The angles form a triangle";
+  if (inputs[0].value == "" || inputs[1].value == "" || inputs[2].value == "") {
+    output.innerText = "Please enter all the inputs!";
+  } else if (
+    inputs[0].value <= 0 ||
+    inputs[1].value <= 0 ||
+    inputs[2].value <= 0
+  ) {
+    output.innerText = "Inputs should be greater than zero!";
   } else {
-    output.innerText = "Oh no! The angles don't form a triangle";
+    const sumOfAngles = calculateSum(
+      Number(inputs[0].value),
+      Number(inputs[1].value),
+      Number(inputs[2].value)
+    );
+
+    if (sumOfAngles === 180) {
+      output.innerText = "Yay! The angles form a triangle";
+    } else {
+      output.innerText = "Oh no! The angles don't form a triangle";
+    }
   }
 }
 
